09eb4072096381132704f7065b5e96aa0429ef52
bartbie <bartbie37@gmail.com>
2023-04-26 00:11:25 +0200

    fix(auth): removed redundant option

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -41,8 +41,6 @@ export const authOptions: NextAuthOptions = {
     signIn: "/auth/signin",
   },
   callbacks: {
-    redirect: async ({url, baseUrl}) => {},
-
     session: ({ session, user }) => ({
       ...session,
       user: {
